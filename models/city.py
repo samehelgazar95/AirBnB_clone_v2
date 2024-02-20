0d38c85ad19adf911841f636a357ffe9ced82c78
Sameh ElGazar <selgazar95@gmail.com>
2024-02-20 20:21:58 +0200

    Storage

--- models/city.py
+++ models/city.py
@@ -1,15 +1,24 @@
 #!/usr/bin/python3
 """City class model"""
-from models.base_model import BaseModel
+from os import getenv
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
-class City(BaseModel):
+class City(BaseModel, Base):
     """City class that inherits
         from BaseModel class
         Arguments:
             state_id: State id
             name: City name
         """
+    storage_type = getenv('HBNB_TYPE_STORAGE')
 
-    state_id: str = ''
-    name: str = ''
+    if storage_type == 'file':
+        state_id: str = ''
+        name: str = ''
+    elif storage_type == 'db':
+        __tablename__ = 'cities'
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
--- models/state.py
+++ models/state.py
@@ -1,13 +1,31 @@
 #!/usr/bin/python3
 """State class model"""
-from models.base_model import BaseModel
+from os import getenv
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
+from models.city import City
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """State class that inherits
         from BaseModel class
         Arguments:
             name: State name
         """
+    storage_type = getenv('HBNB_TYPE_STORAGE')
 
-    name: str = ''
+    if storage_type == 'file':
+        name: str = ''
+        
+        @property
+        def cities(self):
+            """ Returns all cities related to this state """
+            from models.__init__ import storage
+            all_cities = storage.all(City)
+            cities = [city for city in all_cities.values() if city.state_id == self.id]
+            return cities
+    elif storage_type == 'db':
+        __tablename__ = 'states'
+        name = Column(String(128), nullable=False)   
+        cities = relationship('City', backref='state', cascade='all, delete, delete-orphan')
