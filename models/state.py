41b395e673f3f88830956295676806ca418fffbc
Sameh ElGazar <selgazar95@gmail.com>
2024-02-20 21:31:11 +0200

    Storage

--- models/state.py
+++ models/state.py
@@ -15,12 +15,12 @@ class State(BaseModel, Base):
         """
     storage_type = getenv('HBNB_TYPE_STORAGE')
 
-    if storage_type == 'db':
-        __tablename__ = 'states'
-        name = Column(String(128), nullable=False)   
-        cities = relationship('City', backref='state', cascade='all, delete, delete-orphan')
-    else:
-        name: str = ''
+    # if storage_type == 'db':
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)   
+    cities = relationship('City', backref='state', cascade='all, delete, delete-orphan')
+    # else:
+    name: str = ''
         
     def __init__(self, *args, **kwargs):
         """initializes state"""
