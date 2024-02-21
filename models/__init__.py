43d4a10f677787d3fe03ea3163b2324aca3bebed
Sameh ElGazar <selgazar95@gmail.com>
2024-02-21 17:36:01 +0200

    Terminal 16

--- models/__init__.py
+++ models/__init__.py
@@ -8,7 +8,7 @@ from os import getenv
 
 storage_type = getenv('HBNB_TYPE_STORAGE')
 
-if str(storage_type) == 'db':
+if storage_type== 'db':
     from models.engine.db_storage import DBStorage
     storage = DBStorage()
 else:
