3aafd9a5299b05f99366a87f2800e1c13f8d8299
Sameh ElGazar <selgazar95@gmail.com>
2024-02-21 21:33:17 +0200

    -4

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -37,7 +37,7 @@ class DBStorage():
 
         print('>>> db_storage.py ----- __init__ <<<')
         print(db_url)
-        DBStorage.__engine = create_engine(db_url, pool_pre_ping=True)
+        self.__engine = create_engine(db_url, pool_pre_ping=True)
         if getenv('HBNB_ENV') == 'test':
             Base.metadata.drop_all(self.__engine)
         
