50875d44b8862f517eb0b12bc29b76d89fc67d98
Sameh ElGazar <selgazar95@gmail.com>
2024-02-22 11:32:02 -0800

    Fixing syntax in Use

--- models/user.py
+++ models/user.py
@@ -9,7 +9,7 @@ class User(BaseModel, Base):
 
     if models.storage_type == 'db':
         __tablename__ = 'users'
-        email = Column(String(128, nullable=False))
+        email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=False)
         last_name = Column(String(128), nullable=False)
