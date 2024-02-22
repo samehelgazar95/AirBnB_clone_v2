a101467b81fdf0d1a55f5e7aaf0812fa8cf5e3be
Sameh ElGazar <selgazar95@gmail.com>
2024-02-22 04:21:51 +0200

    Task 5 create delete, updating all

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-"""The centeralized storage for the application
-almost followed the singleton pattern"""
+"""
+The centeralized storage for the application
+almost followed the singleton pattern
+"""
 import json
 import os
 from models.base_model import BaseModel
@@ -13,23 +15,30 @@ from models.review import Review
 
 
 class FileStorage:
-    """Manipulate the file that's storing data
-        Arguments:
-            __file_path: to store the JSON format
-            __objects: {"ClassName.id": Object, "ClassName.id": Object}
-            models_map: mapping the models for easy accessing
+    """
+    Manipulate the file that's storing data
+    Arguments:
+        __file_path: to store the JSON format
+        __objects: {"ClassName.id": Object, "ClassName.id": Object}
+        models_map: mapping the models for easy accessing
     """
     __file_path = "file.json"
     __objects = {}
-    models_map = {
-                'BaseModel': BaseModel, 'Amenity': Amenity,
-                'City': City, 'Place': Place, 'Review': Review,
-                'State': State, 'User': User
-            }
+    models_map = {'BaseModel': BaseModel, 'Amenity': Amenity,
+                  'City': City, 'Place': Place, 'Review': Review,
+                  'State': State, 'User': User}
 
-    def all(self):
-        """Simple function returns the private __objects"""
-        return FileStorage.__objects
+    def all(self, cls=None):
+        """returns the private __objects or all objects"""
+        if cls:
+            temp = {}
+            for key, val in FileStorage.__objects.items():
+                key_name = str(key.split('.')[0])
+                if cls.__name__ == key_name:
+                    temp[key] = val
+            return temp
+        else:
+            return FileStorage.__objects
 
     def new(self, obj):
         """Sets in __objects the obj with key <obj class name>.id"""
@@ -43,6 +52,13 @@ class FileStorage:
             data[key] = obj.to_dict()
         with open(FileStorage.__file_path, 'w') as file:
             json.dump(data, file, indent=4)
+
+    def delete(self, obj=None):
+        """Delete object from __objects"""
+        if obj:
+            key = obj.to_dict()['__class__'] + '.' + obj.id
+            del FileStorage.__objects[key]
+        self.save()
 
     def reload(self):
         """Loading the dict from file.json"""
