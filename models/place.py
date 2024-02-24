e27f0e07b2e17172a4e2a889db88a1fa1808ad3b
Sameh ElGazar <selgazar95@gmail.com>
2024-02-24 02:56:12 +0200

    Repair place.py place_amenity table

--- models/place.py
+++ models/place.py
@@ -6,24 +6,25 @@ from sqlalchemy.orm import relationship
 import models
 
 
-place_amenity = Table(
-    'place_amenity',
-    Base.metadata,
-    Column(
-        'place_id',
-        String(60),
-        ForeignKey('places.id'),
-        primary_key=True,
-        nullable=False,
-        ),
-    Column(
-        'amenity_id',
-        String(60),
-        ForeignKey('amenities.id'),
-        primary_key=True,
-        nullable=False,
-        ),
-    )
+if models.storage_type == 'db':
+    place_amenity = Table(
+        'place_amenity',
+        Base.metadata,
+        Column(
+            'place_id',
+            String(60),
+            ForeignKey('places.id'),
+            primary_key=True,
+            nullable=False,
+            ),
+        Column(
+            'amenity_id',
+            String(60),
+            ForeignKey('amenities.id'),
+            primary_key=True,
+            nullable=False,
+            ),
+        )
 
 
 class Place(BaseModel, Base):
