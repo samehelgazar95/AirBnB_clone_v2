aff63fddb02c314aefa298487131899e20a01378
Sameh ElGazar <selgazar95@gmail.com>
2024-02-24 02:36:39 +0200

    Delete the delete

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -55,13 +55,6 @@ class FileStorage:
         with open(FileStorage.__file_path, 'w') as file:
             json.dump(data, file, indent=4)
 
-    def delete(self, obj=None):
-        """Delete object from __objects"""
-        if obj is not None:
-            key = obj.to_dict()['__class__'] + '.' + obj.id
-            del FileStorage.__objects[key]
-        self.save()
-
     def reload(self):
         """Loading the dict from file.json"""
         if os.path.isfile(FileStorage.__file_path):
