a71dfdfd17ed597070e5979d805c02ebc9c4fb78
Sameh ElGazar <selgazar95@gmail.com>
2024-03-06 18:32:26 -0800

    tgz

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,4 +1,32 @@
 #!/usr/bin/python3
 """Yel3an abo el Fabric3==1.14.post1 tany"""
+from fabric.api import local
+from datetime import datetime
 
 
+def do_pack():
+    versions_dir = local("mkdir -p versions")
+    if versions_dir.failed:
+        print("Failed to create versions")
+        return None
+    else:
+        print("     versions_dir created!")
+
+    n = datetime.utcnow()
+    tgz_file = "web_static_{}{}{}{}{}{}.tgz".format(
+            n.year, n.month, n.day, n.hour, n.minute, n.second)
+
+    tgz_cmd = "tar -cvzf versions/{} web_static".format(tgz_file)
+
+    compress = local(tgz_cmd)
+    if compress.failed:
+        print("Failed to compress")
+        return None
+    else:
+        print("     Compressed successfuly!")
+
+    return tgz_file
+
+
+if __name__ == "__main__":
+    do_pack()
