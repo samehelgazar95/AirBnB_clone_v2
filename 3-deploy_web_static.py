5d5dd4fcd5b6cc97d4950e56d015cb227877daa7
Sameh ElGazar <selgazar95@gmail.com>
2024-03-07 11:23:14 -0800

    Full deploy

---
+++ 3-deploy_web_static.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Hn3mel full deploy """
+from fabric.api import local
+
+
+def deploy():
+    # Archiving the web_static files
+    pack = local('fab -f 1-pack_web_static.py do_pack')
+    if pack is None:
+        return False
+
+    # Deploying the files to servers
+    d = '2-do_deploy_web_static.py'
+    dep_code = 'fab -f {} do_deploy:archive_path=versions/{}'.format(d, pack)
+    deploy = local(dep_code)
+    return deploy
