24c0700aefa85e951c381e2b9206af9f8cc4dd6c
Sameh ElGazar <selgazar95@gmail.com>
2024-03-08 18:54:13 +0200

    fuck it

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ Deploy Module for Web Static Content """
-from os.path import isfile
+from os.path import exists
 from fabric.api import env, put, run
 
 
@@ -17,28 +17,25 @@ def do_deploy(archive_path):
         If the file doesn't exist at archive_path or an error occurs - False.
         Otherwise - True.
     """
-    if isfile(archive_path) is False:
+    if exists(archive_path) is False:
         return False
 
     _file = archive_path.split('/')[-1]
     _name = _file.split('.')[0]
+    path = '/data/web_static/releases/'
 
     try:
-        put(archive_path, '/tmp/{}'.format(_file))
-        run('sudo mkdir -p /data/web_static/releases/{}/'.format(_name))
-        run('sudo tar -xzf /tmp/{} -C /data/web_static/releases/{}/'.
-            format(_file, _name))
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}{}/'.format(path, _name))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(_file, path, _name))
+        run('rm /tmp/{}'.format(_file))
 
-        run('sudo mv /data/web_static/releases/{}/web_static/* '
-            '/data/web_static/releases/{}/'.format(_name, _name))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, _name))
 
-        run('sudo rm /tmp/{}'.format(_file))
-        run('sudo rm -rf /data/web_static/releases/{}/web_static'.format(
-            _name))
-        run('sudo rm -rf /data/web_static/current')
+        run('rm -rf {}{}/web_static'.format(path, _name))
+        run('rm -rf /data/web_static/current')
 
-        run('sudo ln -sf /data/web_static/releases/{}/'
-            '/data/web_static/current'.format(_name))
+        run('sudo ln -s {}{}/ /data/web_static/current'.format(path, _name))
         return True
     except Exception:
         return False
