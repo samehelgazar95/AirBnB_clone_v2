33f1bfdbd519b80b8b15b1eca7c7e8b622add576
Sameh ElGazar <selgazar95@gmail.com>
2024-03-08 07:56:27 -0800

    Yel3an abo el Fabric TAlet

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -9,9 +9,13 @@ env.user = 'ubuntu'
 
 
 def do_deploy(archive_path):
-    """
-    Do some Sha2labzat and make the deploy
-    Function documentaion
+    """Distributes an archive to a web server.
+
+    Args:
+        archive_path (str): The path of the archive to distribute.
+    Returns:
+        If the file doesn't exist at archive_path or an error occurs - False.
+        Otherwise - True.
     """
     if isfile(archive_path) is False:
         return False
