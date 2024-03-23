69a2320647c735d55d8bd1c93cb52b5f2991b07a
Sameh ElGazar <selgazar95@gmail.com>
2024-03-23 02:56:49 +0200

    Task 2:

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -3,7 +3,7 @@
 Initiate Flask
 - Create route to /
 - Create route to hbnb /hbnb
-- 
+- Create route to /c/<text>
 """
 from flask import Flask
 
@@ -13,14 +13,21 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def home():
-    """ Creating the home route """
+    """ route to home """
     return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """ Creating the hbnb route """
+    """ route to hbnb route """
     return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_url(text):
+    """ route to /c/<text> """
+    text_val = text.replace('_', ' ')
+    return "C {}".format(text_val)
 
 
 if __name__ == "__main__":
