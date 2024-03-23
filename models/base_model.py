355a24bd4215b71e224b719c504e49eabc7c8d56
Sameh ElGazar <selgazar95@gmail.com>
2024-03-23 15:13:33 +0200

    Style 7.1

--- models/base_model.py
+++ models/base_model.py
@@ -25,9 +25,9 @@ class BaseModel:
 
     if storage_type == 'db':
         id = Column(String(60), primary_key=True, nullable=False)
-        created_at = Column(DateTime, default=datetime.now(datetime.UTC),
+        created_at = Column(DateTime, default=datetime.utcnow(),
                             nullable=False)
-        updated_at = Column(DateTime, default=datetime.now(datetime.UTC),
+        updated_at = Column(DateTime, default=datetime.utcnow(),
                             nullable=False)
 
     def __init__(self, *args, **kwargs):
