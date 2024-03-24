ab8ebdc48f76f5f04fd3a4a19ea49ea788dfa116
Sameh ElGazar <selgazar95@gmail.com>
2024-03-24 13:25:37 -0700

    100

--- web_flask/100-hbnb.py
+++ web_flask/100-hbnb.py
@@ -8,7 +8,7 @@ from flask import Flask, render_template
 from models import storage
 from models.state import State
 from models.amenity import Amenity
-
+from models.place import Place
 
 app = Flask(__name__)
 
@@ -24,8 +24,9 @@ def hbnb():
     """ route to /hbnb """
     states = storage.all(State)
     amenities = storage.all(Amenity)
+    places = storage.all(Place)
     return render_template('100-hbnb.html', states=states,
-                           amenities=amenities)
+                           amenities=amenities, places=places)
 
 
 @app.teardown_appcontext
